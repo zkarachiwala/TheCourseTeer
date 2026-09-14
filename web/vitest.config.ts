a2df6b3b5ff9ec9450c0db2fd3507648da708328
Zak Karachiwala <31942040+zkarachiwala@users.noreply.github.com>
2026-09-15 07:57:52 +1000

    Fix vitest config to use setupFilesAfterEnv instead of setupFiles

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFilesAfterEnv: ['./src/test/setup.ts'],
   },
   resolve: {
     alias: {
